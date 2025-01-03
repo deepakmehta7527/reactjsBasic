dc3fcf5b00ea99a90a2a4f576b8f0b32d27262a0
Deepakmehta64 <deepakmehta7527@gmail.com>
2025-01-03 19:04:03 +0530

    controlled components handling form

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ hona chahiye ,aggr hota hai tho react use krne ka kya fayada
 1)we have three way to handle "form"
 a)useRef= ess tareeke mein hum har input ko select kar lete hai and unki value tab nikaalte hai jab form
 submit hota hai
-b)controlled components =>
+
+b)controlled components => aap jabhi kuch likho "useState" ke through data real time par update kar dein jaise hi
+kuch type ho set state kardo nayi value ke barabar
+
+
 c)react hook form
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import React, { useRef } from 'react'
-const App = () => {
-  // by default pass null it compulsory
-  const surname=useRef(null);
-  const name=useRef(null)
+import React, { useState } from 'react'
 
-  // ye ek function hai jha pe form ko control ke saat "output" bhi "nikal" skte hia
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    // console.log(name.current.value);
-    console.log(surname.current.value,name.current.value);
+const App = () => {
+
+  const [val,setval]=useState({name:"",email:""});
+
+  const handlesubmit=(event)=>{
+    event.preventDefault();
+    console.log(val);
   }
 
   return (
-    // "onSubmit" se form ko by default referece ko roka ja skta hai
-   <form onSubmit={handleSubmit}>
-    {/* "ref" use krke har ek input ko target kiya jata hai aur jab
-    "form submit" hoga tab input "nikal" skte hai  */}
-    <input ref={name} type="text" placeholder='name' />
-    <input ref={surname} type="text" placeholder='surname' />
-    <input type="submit" />
-   </form>
+    <div>
+<form onSubmit={handlesubmit}>
+  {/* jab koi input update ho tho uske saat previous value pe effect na pade tho "previous value" store
+   ke saat new value bhi "ui" pe display ho tho ess type se use kre */}
+<input type="text" placeholder='name' onChange={(event)=>setval({...val,name:event.target.value})}/>
+<input type="email" placeholder='email' onChange={(event)=>setval({...val,email:event.target.value})}  />
+<input type="submit" />
+</form>
+    </div>
   )
 }
 
